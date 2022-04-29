02f89aabd4443c63bf77bd56ff6c8595b63d1ff3
yashramani7 <yash.209301616@muj.manipal.edu>
2022-04-29 12:50:26 +0530

    Typeface: Add mono and product sans font

--- app/src/main/java/com/beesechurgers/gullak/ui/theme/Type.kt
+++ app/src/main/java/com/beesechurgers/gullak/ui/theme/Type.kt
@@ -2,9 +2,14 @@ package com.beesechurgers.gullak.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.beesechurgers.gullak.R
+
+val googleSansFont = FontFamily(Font(R.font.google_sans))
+val monoFont = FontFamily(Font(R.font.jet_brains_mono))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
