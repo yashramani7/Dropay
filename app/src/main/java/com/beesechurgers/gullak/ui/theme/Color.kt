da22b24d3fb19f654cb0c09ed2f6843a0bc184fc
yashramani7 <yash.209301616@muj.manipal.edu>
2022-04-29 12:44:48 +0530

    Add themes and colors

--- app/src/main/java/com/beesechurgers/gullak/ui/theme/Color.kt
+++ app/src/main/java/com/beesechurgers/gullak/ui/theme/Color.kt
@@ -1,8 +1,26 @@
 package com.beesechurgers.gullak.ui.theme
 
+import android.content.Context
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+@Composable
+fun backgroundColor() =
+    colorResource(id = if (isSystemInDarkTheme()) android.R.color.system_neutral1_900 else android.R.color.system_neutral1_10)
+
+@Composable
+fun strokeColor(): Color =
+    if (isSystemInDarkTheme()) Color(0xFF444444) else Color(0xFFDADADA)
+
+@Composable
+fun navigationBarColor(ctx: Context) =
+    if (isSystemInDarkTheme()) dynamicDarkColorScheme(ctx).secondaryContainer.copy(alpha = 0.31f)
+    else dynamicLightColorScheme(ctx).secondaryContainer.copy(alpha = 0.31f)
+
+@Composable
+fun fabColor() =
+    colorResource(id = if (isSystemInDarkTheme()) android.R.color.system_accent1_200 else android.R.color.system_accent1_600)
