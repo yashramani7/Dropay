da3a655bebf15293b0ebbad5f4a347afc04394f1
yashramani7 <yash.209301616@muj.manipal.edu>
2022-04-30 11:17:06 +0530

    Implement QR code detection

--- app/src/main/java/com/beesechurgers/gullak/QRScanActivity.kt
+++ app/src/main/java/com/beesechurgers/gullak/QRScanActivity.kt
@@ -1,6 +1,7 @@
 package com.beesechurgers.gullak
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -16,6 +17,7 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
@@ -26,13 +28,13 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.beesechurgers.gullak.ui.theme.GullakTheme
 import com.beesechurgers.gullak.ui.theme.backgroundColor
+import com.beesechurgers.gullak.ui.theme.googleSansFont
 import com.beesechurgers.gullak.utils.QRCodeAnalyzer
 
 class QRScanActivity : ComponentActivity() {
@@ -42,82 +44,114 @@ class QRScanActivity : ComponentActivity() {
         setContent {
             window.statusBarColor = backgroundColor().toArgb()
             GullakTheme(context = this) {
-                Surface(
+                Scaffold(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(color = backgroundColor())
-                        .animateContentSize(),
-                    color = backgroundColor(),
+                        .background(color = backgroundColor()),
                     contentColor = contentColorFor(backgroundColor = backgroundColor()),
-                ) {
-                    val context = LocalContext.current
-                    val lifecycleOwner = LocalLifecycleOwner.current
-                    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
-
-                    var hasCameraPermission by remember {
-                        mutableStateOf(
-                            ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-                        )
-                    }
-                    val launcher = rememberLauncherForActivityResult(
-                        contract = ActivityResultContracts.RequestPermission(),
-                        onResult = { granted ->
-                            hasCameraPermission = granted
-                        }
-                    )
-
-                    LaunchedEffect(key1 = true) {
-                        launcher.launch(Manifest.permission.CAMERA)
-                    }
-
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        if (hasCameraPermission) {
+                    topBar = {
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(color = backgroundColor())
+                                .animateContentSize(),
+                            color = backgroundColor(),
+                            contentColor = contentColorFor(backgroundColor = backgroundColor()),
+                        ) {
                             Text(
                                 text = "Scan QR",
                                 fontSize = 24.sp,
+                                fontFamily = googleSansFont,
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .padding(start = 32.dp, top = 32.dp)
+                                    .padding(start = 16.dp, top = 32.dp)
                             )
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .align(Alignment.CenterHorizontally)
-                            ) {
-                                AndroidView(
-                                    factory = { context ->
-                                        val previewView = PreviewView(context)
-                                        val preview = Preview.Builder().build()
-                                        val selector = CameraSelector.Builder()
-                                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                                            .build()
-                                        preview.setSurfaceProvider(previewView.surfaceProvider)
-                                        val imageAnalysis = ImageAnalysis.Builder()
-                                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                                            .build()
-                                        imageAnalysis.setAnalyzer(
-                                            ContextCompat.getMainExecutor(context),
-                                            QRCodeAnalyzer { result ->
-                                                // TODO: handle result
-                                            }
-                                        )
-
-                                        try {
-                                            cameraProviderFuture.get().bindToLifecycle(lifecycleOwner, selector, preview, imageAnalysis)
-                                        } catch (e: Exception) {
-                                            e.printStackTrace()
-                                        }
-
-                                        return@AndroidView previewView
-                                    },
-                                    modifier = Modifier
-                                        .align(Alignment.CenterVertically)
-                                        .fillMaxWidth()
-                                        .padding(start = 16.dp, end = 16.dp)
-                                        .clip(RoundedCornerShape(24.dp))
-                                )
-                            }
                         }
+                    },
+                    content = { Content() }
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Content() {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = backgroundColor())
+                .animateContentSize(),
+            color = backgroundColor(),
+            contentColor = contentColorFor(backgroundColor = backgroundColor()),
+        ) {
+            val context = LocalContext.current
+            val lifecycleOwner = LocalLifecycleOwner.current
+            val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+            var hasCameraPermission by remember {
+                mutableStateOf(
+                    ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                )
+            }
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission(),
+                onResult = { granted ->
+                    hasCameraPermission = granted
+                }
+            )
+
+            LaunchedEffect(key1 = true) {
+                launcher.launch(Manifest.permission.CAMERA)
+            }
+
+            Column(modifier = Modifier.fillMaxSize()) {
+                if (hasCameraPermission) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        AndroidView(
+                            factory = { context ->
+                                val previewView = PreviewView(context)
+                                val preview = Preview.Builder().build()
+                                val selector = CameraSelector.Builder()
+                                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                    .build()
+                                preview.setSurfaceProvider(previewView.surfaceProvider)
+                                val imageAnalysis = ImageAnalysis.Builder()
+                                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                    .build()
+                                imageAnalysis.setAnalyzer(
+                                    ContextCompat.getMainExecutor(context),
+                                    QRCodeAnalyzer { result ->
+                                        result?.let {
+                                            val tokens = it.split(":")
+                                            if (tokens.size != 2) return@let
+                                            if (TransactionPaymentActivity.mOpened) return@let
+                                            TransactionPaymentActivity.mOpened = true
+                                            startActivity(
+                                                Intent(this@QRScanActivity, TransactionPaymentActivity::class.java)
+                                                    .putExtra("title", tokens[0]).putExtra("_amount", tokens[1])
+                                            )
+                                        }
+                                    }
+                                )
+
+                                try {
+                                    cameraProviderFuture.get().bindToLifecycle(lifecycleOwner, selector, preview, imageAnalysis)
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                }
+
+                                return@AndroidView previewView
+                            },
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, end = 16.dp)
+                                .clip(RoundedCornerShape(24.dp))
+                        )
                     }
                 }
             }
